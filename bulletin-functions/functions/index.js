23bda99c0afa2df668cc2ed994a9fb0c88223108
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2020-10-01 13:48:03 -0700

    express api

--- bulletin-functions/functions/index.js
+++ bulletin-functions/functions/index.js
@@ -3,31 +3,28 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello World!");
-});
+const express = require('express');
+const app = express();
 
-exports.getPins = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('pins').get()
+app.get('/pins', (req, res) => {
+    admin
+        .firestore()
+        .collection('pins')
+        .get()
         .then(data => {
             let pins = [];
             data.forEach(doc => {
                 pins.push(doc.data());
-
             });
             return res.json(pins);
-        })
-        .catch(err => console.error(err));
-})
+            })
+            .catch(err => console.error(err));
+});
 
-exports.createPin = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: 'Method not allowed.'});
-    }
+
+
+
+app.post('/pin', (req, res) => {
     const newPin = {
         body: req.body.body,
         userHandle: req.body.userHandle,
@@ -46,3 +43,7 @@ exports.createPin = functions.https.onRequest((req, res) => {
             console.error(err);
         })
 });
+
+// https://baseurl.com/api/screams
+
+exports.api = functions.https.onRequest(app);
